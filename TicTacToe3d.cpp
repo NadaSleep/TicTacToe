76541511e3129f6448fe37a12a52aefb0ede75cc
NadaSleep <tyler.m.sexton@gmail.com>
2024-04-04 18:17:21 -0400

    Added method to check for winner on one side

--- TicTacToe3d.cpp
+++ TicTacToe3d.cpp
@@ -123,7 +123,6 @@ return ' ';
     while (gameBoard[x][y] != ' '); 
   }
 
-
 //Keeps track of open spaces
   int checkOpenSpaces()
   {
@@ -216,7 +215,7 @@ return ' ';
 
 
 
-//New Thang
+//New Thang Thang
 
 class TicTacToeThreeD : public TicTacToe
 { private:
@@ -260,6 +259,39 @@ void showBoard3D()
    cout << endl;
   }
 
+//Checks to see if there is a winner
+  char winnerCheck()
+  {
+//rows    
+	for(int i = 0; i < 3; i++)
+  	{
+	  if(gameBoard[i][0][] != '*' && gameBoard[i][0][] == gameBoard[i][1][] && gameBoard[i][0][] == gameBoard[i][2][])
+	  {
+
+	  	return gameBoard[i][0][];
+	  }
+	
+	}
+//columns  	
+  	for(int i = 0; i < 3; i++)
+  	{
+	  if(gameBoard[0][i][] != '*' && gameBoard[0][i][] == gameBoard[1][i][] && gameBoard[0][i][] == gameBoard[2][i][])
+	  {
+
+	    return gameBoard[0][i][];
+	  }
+	}
+//diagonals	
+    if(gameBoard[0][0][] != '*' && gameBoard[0][0][] == gameBoard[1][1][] && gameBoard[0][0][] == gameBoard[2][2][])	
+    {
+      return gameBoard[0][0][];
+	} 	
+    if(gameBoard[0][2][] != '*' && gameBoard[0][2][] == gameBoard[1][1][] && gameBoard[0][2][] == gameBoard[2][0][]) 
+    {
+	  return gameBoard[0][2][];
+	}
+return ' ';    
+  }
 
 
 
